55d432c737349fa44fbc72dea7c252dc59f2a1bb
PRABUDH TYAGI <prabudhtyagi@gmail.com>
2026-02-25 15:56:44 +0530

    Add files via upload

---
+++ aaarmStrong.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int num,rem,sum,temp,i;
+    printf("Armstrong number from 1 to 100 are : \n");
+    for(i=1;i<=100;i++){
+        temp = num;
+        sum = 0;
+        while (temp !=0){
+
+            rem = temp%10;
+            sum = sum + rem*rem*rem;
+            temp = temp/10;
+        }
+        if(sum==num){
+            printf("%d",num);
+        }
+        
+    }
+    return 0;
+}
