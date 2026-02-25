7ee5c9217eecc63bcd4097fb322d66ee5ed8274b
PRABUDH TYAGI <prabudhtyagi@gmail.com>
2026-02-25 16:24:35 +0530

    Add files via upload

--- quadEquation.c
+++ quadEquation.c
@@ -1,4 +1,30 @@
-#include<stdio.h>
-int main(){
-    
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float a, b, c;
+    float d, root1, root2;
+
+    printf("Enter coefficients a, b and c: ");
+    scanf("%f %f %f", &a, &b, &c);
+
+    d = b*b - 4*a*c;   
+    printf("the roots are %f",d);
+
+    if (d > 0) {
+        root1 = (-b + sqrt(d)) / (2*a);
+        root2 = (-b - sqrt(d)) / (2*a);
+        printf("Roots are real and different\n");
+        printf("Root1 = %.2f\nRoot2 = %.2f", root1, root2);
+    }
+    else if (d == 0) {
+        root1 = -b / (2*a);
+        printf("Roots are real and equal\n");
+        printf("Root = %.2f", root1);
+    }
+    else {
+        printf("Roots are imaginary (no real roots)");
+    }
+
+    return 0;
 }
