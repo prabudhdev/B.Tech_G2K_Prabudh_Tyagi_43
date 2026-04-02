ccc7babac4e3b874bc2ae2e2f195a2d7bb6600e4
PRABUDH TYAGI <prabudhtyagi@gmail.com>
2026-04-02 14:37:15 +0530

    Update aaarmStrong.c

--- aaarmStrong.c
+++ aaarmStrong.c
@@ -7,19 +7,35 @@ int main(){
     int num,rem,sum,temp,i; 
 
     printf("Armstrong number from 1 to 100 are : \n");
-    for(i=1;i<=100;i++){
+
+    for(i=1;i<=100;i++)
+
+    {
+
         temp = num;
+
         sum = 0;
-        while (temp !=0){
+
+        while (temp !=0)
+
+      {
 
             rem = temp%10;
+
             sum = sum + rem*rem*rem;
+
             temp = temp/10;
+
         }
+
         if(sum==num){
+
             printf("%d",num);
+
         }
         
     }
+
     return 0;
+
 }
