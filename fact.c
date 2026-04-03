0864a696b42ebb346f55391087b76a82d7f675c2
PRABUDH TYAGI <prabudhtyagi@gmail.com>
2026-04-03 17:59:26 +0530

    Update fact.c

--- fact.c
+++ fact.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
+
 int main (){
-    int fact=1,i,n;
+
+    int fact=1,i,n;// variables declared here 
+
+
     printf("Enter your number:");
-    scanf("%d",&n);
+    scanf("%d",&n);//input from user that store in n variable.
+
     for(i=1;i<=n;i++){
+
         fact = fact*i;
+
     }
+
     printf("%d\n",fact);
+
     return 0;
 }
