ad29f8211e445b67a698cbad39e81a46b9042308
PRABUDH TYAGI <prabudhtyagi@gmail.com>
2026-04-03 18:02:25 +0530

    Update palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -1,20 +1,37 @@
 #include <stdio.h>
-#include<string.h>
+
+#include<string.h>// HEADER FILE for mathematical calculation.
+
 int main(){
     int n,i,reversed=0,original,remainder;
+
     printf("enter a no.:");
     scanf("%d",&n);
+
     original = n;
+
     while(n!=0){
+
         remainder = n % 10;
         reversed= reversed*10 + remainder ;
-        n/=10;
-    }
-   if(original== reversed){
-      printf("%d is a palindrome no.",original);
 
-   }else{
+        n/=10;//this is because of remove the last digit 
+
+    }
+
+   if(original== reversed){
+
+      printf("%d is a palindrome
+ no.",original);
+
+
+   }else
+{
+
      printf("%d is  not a palindrome no.",original);
+
    }
+
    return 0;
+
 }
