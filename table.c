3e405376555653bc5f007b2a4b720131f20a6e29
PRABUDH TYAGI <prabudhtyagi@gmail.com>
2026-04-03 18:03:09 +0530

    Update table.c

--- table.c
+++ table.c
@@ -1,11 +1,20 @@
 #include<stdio.h>
+
 int main (){
+
     int i,n,table;
+
     printf("Enter your number :");
+
     scanf("%d",&n);
+
     for(i=1;i<=10;i++){
+
         table = i*n;
-        printf("%d X %d = %d\n",n,i,table);
+
+        printf("%d X %d =
+ %d\n",n,i,table);
+
     }
-   
+   return 0;
 }
