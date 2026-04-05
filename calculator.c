73312ca148e46f4518cbdfdfe9950b4c8c4de48e
PRABUDH TYAGI <prabudhtyagi@gmail.com>
2026-04-05 18:50:23 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -1,31 +1,47 @@
 #include<stdio.h>
+
 int main(){
+
     char op;
+
     double a,b,res;
+
     printf("enter operators(+,-,*,/):");
     scanf("%c",&op);
+
     printf("enters two no.:");
     scanf("%lf %lf",&a,&b);
-    switch(op){
+
+    switch(op)
+{
+
         case '+':
         res =a+b;
         break;  
+
         case '-':
         res =a-b;
         break;
+
         case '*':
         res =a*b;
         break;
+
         case '/':
         if(b !=0) {
             res = a/b;
         }else{
             printf("Eror! 0 se devide nahi hoga.");
             break;
+
             default:
             printf("Worng operator!");
+
         }
+
 }
-printf("Result : %.2lf",res);
+
+printf("Result : %.2lf",res);//yha print hoga result 
 return 0;
+
 }
