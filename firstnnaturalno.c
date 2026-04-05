e2f7fd28a5599ff9e35f26fe574d8153abd3fe37
PRABUDH TYAGI <prabudhtyagi@gmail.com>
2026-04-05 18:48:06 +0530

    Update firstnnaturalno.c

--- firstnnaturalno.c
+++ firstnnaturalno.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+
 int main (){
+
     int i,n;
-    printf("Enter your no. : ");
+
+    printf("Enter your no.: ");
     scanf("%d",&n);
+
     for(i=1;i<=n;i++){
           printf("%d\n",i);
     }
     
     return 0;
 }
+//output will be 1 to n .. 
