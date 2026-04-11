bc14c2d3f0c24b9d7c3e3c01ced93f461ac087cf
PRABUDH TYAGI <prabudhtyagi@gmail.com>
2026-04-11 17:58:10 +0530

    Update AddTwoArry.c

--- AddTwoArry.c
+++ AddTwoArry.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 
 int main() {
+
     int arr1[] = {1, 2, 3, 4, 5};
+
     int arr2[] = {5, 4, 3, 2, 1};
+
     int n = sizeof(arr1) / sizeof(arr1[0]); // number of elements
+
     int sum[n]; // array to store the sum
 
     // Add corresponding elements
+
     for (int i = 0; i < n; i++) {
-        sum[i] = arr1[i] + arr2[i];
-    } // sum of two array and store in a sum variable
+        sum[i] = arr1[i] + arr2[i]
+    }; // sum of two array and store in a
+ sum variable
 
     // Print the result
     printf("Sum of two arrays: ");
