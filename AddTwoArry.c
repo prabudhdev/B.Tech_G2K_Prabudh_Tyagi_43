d7123a3ba623fc5e084353567e4e06cdf4a14e45
PRABUDH TYAGI <prabudhtyagi@gmail.com>
2026-07-04 22:22:03 +0530

    Update array initialization and enhance comments
    Refactor array initialization and improve comments.

--- AddTwoArry.c
+++ AddTwoArry.c
@@ -2,27 +2,32 @@
 
 int main() {
 
-    int arr1[] = {1, 2, 3, 4, 5};
+    int arr1[] = {1, 2, 3, 4, 5};// assigned the array the values
 
-    int arr2[] = {5, 4, 3, 2, 1};
+    int arr2[] = {5, 4, 3, 2, 1};// assigned the array the values
 
     int n = sizeof(arr1) / sizeof(arr1[0]); // number of elements
 
     int sum[n]; // array to store the sum
 
-    // Add corresponding elements
+ 
 
     for (int i = 0; i < n; i++) {
+        
         sum[i] = arr1[i] + arr2[i]
-    }; // sum of two array and store in a
- sum variable
+             
+    }; // sum of two array and store in a sum varibale.
+    
+
 
     // Print the result
+    
     printf("Sum of two arrays: ");
-    for (int i = 0; i < n; i++) // this loop is for printing sum value .
-{
+    
+    for (int i = 0; i < n; i++) {
+        
         printf("%d ", sum[i]);
-    }
+    }                                    //here loops print the values.
 
     return 0;
 }
